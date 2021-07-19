9611e60874e316f8e75d12318c255c1a9533a102
omegamaximus10 <bryan_2904@hotmail.com>
2021-07-19 16:05:38 +0000

    ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
+let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let suit = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let top = document.getElementById("top");
+  let bottom = document.getElementById("bottom");
+  top.innerHTML = randomSuit;
+  document.getElementById("number").innerHTML = generateRandomNumber();
+  bottom.innerHTML = randomSuit;
+
+  if (randomSuit == "&hearts;" || randomSuit == "&diams") {
+    top.style.color = "red";
+    bottom.style.color = "red";
+  } else {
+    top.style.color = "black";
+    bottom.style.color = "black";
+  }
 };
+
+function generateRandomNumber() {
+  let randomNum = number[Math.floor(Math.random() * number.length)];
+  return randomNum;
+}
